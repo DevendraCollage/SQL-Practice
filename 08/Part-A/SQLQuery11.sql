e413c45ff46885a8ab6d2dd04a5d294e9ee5240b
Devendra Parmar <119692067+DevendraCollage@users.noreply.github.com>
2024-01-21 13:15:16 +0530

    Update SQLQuery11.sql

--- 08/Part-A/SQLQuery11.sql
+++ 08/Part-A/SQLQuery11.sql
@@ -1,3 +1,3 @@
--- 11. Write a query to convert a string ‘1234.56’ to number (Use cast and convert function).
+-- 11. Write a query to convert a string â€˜1234.56â€™ to number (Use cast and convert function).
 select cast('1234.56' as float) -- Using Cast Function
-select convert(float,'1234.56') -- Using Cinvert Fuction
+select convert(float,'1234.56') -- Using Convert Fuction
